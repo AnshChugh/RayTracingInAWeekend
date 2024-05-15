ef71309ee36654eefa0e920cabb10ad91533c29f
AnshChugh <Anshchugh20@GMAil.com>
2024-05-15 14:37:42 +0530

    refractor camera code to a seperate class

--- main.cpp
+++ main.cpp
@@ -2,27 +2,10 @@
 #include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
-
-
-color ray_color(const ray& r , const hittable& world){
-    hit_record rec;
-    if(world.hit(r,interval(0,infinity) , rec)){
-        return 0.5 * (rec.normal + color(1,1,1)); // normalise [-1,1] to [0,1] for color
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
-}
+#include "camera.h"
 
 
 int main(){
-
-    auto aspect_ratio = 16.0/9.0;
-    int image_width = 1024;
-
-    // calculate image height and make sure it is atleast 1
-    int image_height = int(image_width/aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
 
     // world
 
@@ -31,42 +14,12 @@ int main(){
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // camera
-    // coordinate x to right y to top and -z to viewing side
+    camera cam;
+    cam.aspect_ratio = 16.0/9.0;
+    cam.image_width = 1024;
 
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width)/ image_height);
-    auto camera_center = point3(0,0,0);
+    cam.render();
 
-    // calculate the vectors across horizontal and down the vertical edges
-    auto viewport_u = vec3(viewport_width , 0 , 0);
-    auto viewport_v = vec3(0 , -viewport_height , 0);
-
-    // horizontal and verticle delta vectors
-    auto pixel_delta_u = viewport_u/image_width;
-    auto pixel_delta_v = viewport_v/image_height;
-
-    // calculate the location of upper left pixel
-    // refer to diagram in the book for this part
-    auto viewport_upper_left = camera_center - vec3(0,0, focal_length) - viewport_u/2 - viewport_v/2;
-    auto pixel00loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    //render
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for(int j = 0; j < image_height ;j++){
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for(int i = 0; i< image_width; i++){
-            auto pixel_center = pixel00loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center , ray_direction);
-
-            color pixel_color = ray_color(r,world);
-            write_color(std::cout , pixel_color);
-        }
-    }
-    std::clog << "\rDone.               \n";
 
     return 0;
 }
